7c8044a6cc035d4bfbd75601cbfb8bbad1251f82
Gonzalo Folli <gonzalo.folli@ross.com.ar>
2024-08-20 13:09:00 -0300

    Agregar archivos de proyecto.

---
+++ transportePDFApi/Controllers/ReportController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace transportePDFApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ReportController : ControllerBase
+    {
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        [HttpGet(Name = "GetReport")]
+        public void Get()
+        {
+            //return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            //{
+            //    Date = DateTime.Now.AddDays(index),
+            //    TemperatureC = Random.Shared.Next(-20, 55),
+            //    Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            //})
+            //.ToArray();
+        }
+
+    }
+}
