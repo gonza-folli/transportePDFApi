5c7c69482b9dbaa69cfd6677e88d44d915f72153
Gonzalo Folli <gonzalo.folli@ross.com.ar>
2024-08-21 15:26:16 -0300

    agrego todos los endpoint en el controller

--- transportePDFApi/Controllers/ReportController.cs
+++ transportePDFApi/Controllers/ReportController.cs
@@ -14,49 +14,16 @@ namespace transportePDFApi.Controllers
     [Route("[controller]")]
     public class ReportController : ControllerBase
     {
-        //private readonly IDictionary<string, Action<PdfFormField, string, ReporteCAUModel>> _fieldKeyActionBinding;
         private readonly IReportService _reportService;
 
         public ReportController(IReportService reportService) {
-
-            //_fieldKeyActionBinding = new Dictionary<string, Action<PdfFormField, string, ReporteCAUModel>>
-            //    {
-            //        {"EXPEDIENTE", ReporteCAUHelper.SetExpedienteField},
-            //        {"LOCALIDAD", ReporteCAUHelper.SetLocalidadField},
-            //        {"NROCAU", ReporteCAUHelper.SetNroCauField},
-            //        {"NRO_CAU", ReporteCAUHelper.SetNroCauField},
-            //        {"MODALIDAD", ReporteCAUHelper.SetModalidadField},
-            //        {"CUIT", ReporteCAUHelper.SetCuitField},
-            //        {"DOMINIO", ReporteCAUHelper.SetDominioField},
-            //        {"INSCRIPCION_DNRPA", ReporteCAUHelper.SetInscripcionDNRPAField},
-            //        {"CODIGO_DGT", ReporteCAUHelper.SetCodigoDGTField},
-            //        {"MUNICIPALIDAD", ReporteCAUHelper.SetMunicipalidadField},
-            //        {"LOCACION", ReporteCAUHelper.SetLocacionField},
-            //        {"VENCIMIENTO", ReporteCAUHelper.SetVencimientoField},
-            //        {"MARCA_1", ReporteCAUHelper.SetMarca1Field},
-            //        {"MODELO_1", ReporteCAUHelper.SetModelo1Field},
-            //        {"NRO_1", ReporteCAUHelper.SetNro1Field},
-            //        {"MARCA_2", ReporteCAUHelper.SetMarca2Field},
-            //        {"NRO_2", ReporteCAUHelper.SetNro2Field},
-            //        {"MARCA_3", ReporteCAUHelper.SetMarca3Field},
-            //        {"MODELO_3", ReporteCAUHelper.SetModelo3Field},
-            //        {"ASIENTOS", ReporteCAUHelper.SetAsientosField},
-            //        {"MARCA_4", ReporteCAUHelper.SetMarca4Field},
-            //        {"NRO_4", ReporteCAUHelper.SetNro4Field},
-            //        {"FECHA_EMISION", ReporteCAUHelper.SetFechaEmisionField},
-            //        {"FECHA_VIGENCIA", ReporteCAUHelper.SetFechaVigenciaField}
-            //    };
-
             _reportService = reportService;
-
         }
 
 
-        [HttpGet("ReportCAU",   Name = "GetReportCAU")]
+        [HttpGet("ReportCAUVehiculosMunicipales",   Name = "GetReportCAUVehiculosMunicipales")]
         public IActionResult GetReporteVehiculosMunicipalidades ()
         {
-
-
              /*
              TODO:
             -a. Crear un endpoint de GET generico que reciba "nombre de reporte" a generar
@@ -97,19 +64,167 @@ namespace transportePDFApi.Controllers
                 FECHA_VIGENCIA = new DateTime(2025, 12, 31)
             };
 
-
-
-
-            //byte[] bytes = _reportService.generateReportVehiculoMunicipal(_fieldKeyActionBinding, reporteCAUModel);
-            //byte[] bytes = _reportService.generateReportCauA1(_fieldKeyActionBinding, reporteCAUModel);
             byte[] bytes = _reportService.generateReport(reporteCAUModel, "CAU Vehiculos de Municipalidades (c).pdf");
-            //byte[] bytes = _reportService.generateReportVehiculoMunicipal(_fieldKeyActionBinding, reporteCAUModel);
-            //byte[] bytes = _reportService.generateReportVehiculoMunicipal(_fieldKeyActionBinding, reporteCAUModel);
 
-            return File(bytes, "application/pdf", "nombreDePrueba");
+            return File(bytes, "application/pdf", "CAUVehiculosMunicipales");
 
         }
 
+        [HttpGet("ReportCAUA1", Name = "GetReportCAUA1")]
+        public IActionResult GetReporteCauA1()
+        {
+
+            //Obtengo de la DB el modelo
+            ReporteCAUModel reporteCAUModel = new ReporteCAUModel()
+            {
+                EXPEDIENTE = "EXP-2024-12345",
+                LOCALIDAD = "Buenos Aires",
+                NROCAU = "CAU-67890",
+                NRO_CAU = "CAU-22222",
+                MODALIDAD = "Arrendamiento",
+                CUIT = "20304050607",
+                DOMINIO = "ABC123",
+                INSCRIPCION_DNRPA = "123456789",
+                CODIGO_DGT = "DGT987654",
+                MUNICIPALIDAD = "La Plata",
+                LOCACION = "Avenida Siempreviva 742",
+                VENCIMIENTO = new DateTime(2025, 12, 31),
+                MARCA_1 = "Ford",
+                MODELO_1 = "Fiesta",
+                NRO_1 = "NRO001",
+                MARCA_2 = "Chevrolet",
+                NRO_2 = "NRO002",
+                MARCA_3 = "Toyota",
+                MODELO_3 = "Corolla",
+                ASIENTOS = "5",
+                MARCA_4 = "Volkswagen",
+                NRO_4 = "NRO004",
+                FECHA_EMISION = DateTime.Now,
+                FECHA_VIGENCIA = new DateTime(2025, 12, 31)
+            };
+
+            byte[] bytes = _reportService.generateReport(reporteCAUModel, "CAU Regulares - Titular (a1).pdf");
+
+            return File(bytes, "application/pdf", "CAUA1");
+
+        }
+
+        [HttpGet("ReportCAUA2", Name = "GetReportCAUA2")]
+        public IActionResult GetReporteCauA2()
+        {
+
+            //Obtengo de la DB el modelo
+            ReporteCAUModel reporteCAUModel = new ReporteCAUModel()
+            {
+                EXPEDIENTE = "EXP-2024-12345",
+                LOCALIDAD = "Buenos Aires",
+                NROCAU = "CAU-67890",
+                NRO_CAU = "CAU-22222",
+                MODALIDAD = "Arrendamiento",
+                CUIT = "20304050607",
+                DOMINIO = "ABC123",
+                INSCRIPCION_DNRPA = "123456789",
+                CODIGO_DGT = "DGT987654",
+                MUNICIPALIDAD = "La Plata",
+                LOCACION = "Avenida Siempreviva 742",
+                VENCIMIENTO = new DateTime(2025, 12, 31),
+                MARCA_1 = "Ford",
+                MODELO_1 = "Fiesta",
+                NRO_1 = "NRO001",
+                MARCA_2 = "Chevrolet",
+                NRO_2 = "NRO002",
+                MARCA_3 = "Toyota",
+                MODELO_3 = "Corolla",
+                ASIENTOS = "5",
+                MARCA_4 = "Volkswagen",
+                NRO_4 = "NRO004",
+                FECHA_EMISION = DateTime.Now,
+                FECHA_VIGENCIA = new DateTime(2025, 12, 31)
+            };
+
+            byte[] bytes = _reportService.generateReport(reporteCAUModel, "CAU Regulares - Contrato (a2).pdf");
+
+            return File(bytes, "application/pdf", "CAUA2");
+
+        }
+
+        [HttpGet("ReportCAUB1", Name = "GetReportCAUB1")]
+        public IActionResult GetReporteCauAB1()
+        {
+
+            //Obtengo de la DB el modelo
+            ReporteCAUModel reporteCAUModel = new ReporteCAUModel()
+            {
+                EXPEDIENTE = "EXP-2024-12345",
+                LOCALIDAD = "Buenos Aires",
+                NROCAU = "CAU-67890",
+                NRO_CAU = "CAU-22222",
+                MODALIDAD = "Arrendamiento",
+                CUIT = "20304050607",
+                DOMINIO = "ABC123",
+                INSCRIPCION_DNRPA = "123456789",
+                CODIGO_DGT = "DGT987654",
+                MUNICIPALIDAD = "La Plata",
+                LOCACION = "Avenida Siempreviva 742",
+                VENCIMIENTO = new DateTime(2025, 12, 31),
+                MARCA_1 = "Ford",
+                MODELO_1 = "Fiesta",
+                NRO_1 = "NRO001",
+                MARCA_2 = "Chevrolet",
+                NRO_2 = "NRO002",
+                MARCA_3 = "Toyota",
+                MODELO_3 = "Corolla",
+                ASIENTOS = "5",
+                MARCA_4 = "Volkswagen",
+                NRO_4 = "NRO004",
+                FECHA_EMISION = DateTime.Now,
+                FECHA_VIGENCIA = new DateTime(2025, 12, 31)
+            };
+
+            byte[] bytes = _reportService.generateReport(reporteCAUModel, "CAU Especiales - Titular (b1).pdf");
+
+            return File(bytes, "application/pdf", "CAUB1");
+
+        }
+
+        [HttpGet("ReportCAUB2", Name = "GetReportCAUB2")]
+        public IActionResult GetReporteCauAB2()
+        {
+
+            //Obtengo de la DB el modelo
+            ReporteCAUModel reporteCAUModel = new ReporteCAUModel()
+            {
+                EXPEDIENTE = "EXP-2024-12345",
+                LOCALIDAD = "Buenos Aires",
+                NROCAU = "CAU-67890",
+                NRO_CAU = "CAU-22222",
+                MODALIDAD = "Arrendamiento",
+                CUIT = "20304050607",
+                DOMINIO = "ABC123",
+                INSCRIPCION_DNRPA = "123456789",
+                CODIGO_DGT = "DGT987654",
+                MUNICIPALIDAD = "La Plata",
+                LOCACION = "Avenida Siempreviva 742",
+                VENCIMIENTO = new DateTime(2025, 12, 31),
+                MARCA_1 = "Ford",
+                MODELO_1 = "Fiesta",
+                NRO_1 = "NRO001",
+                MARCA_2 = "Chevrolet",
+                NRO_2 = "NRO002",
+                MARCA_3 = "Toyota",
+                MODELO_3 = "Corolla",
+                ASIENTOS = "5",
+                MARCA_4 = "Volkswagen",
+                NRO_4 = "NRO004",
+                FECHA_EMISION = DateTime.Now,
+                FECHA_VIGENCIA = new DateTime(2025, 12, 31)
+            };
+
+            byte[] bytes = _reportService.generateReport(reporteCAUModel, "CAU Especiales - Contrato (b2).pdf");
+
+            return File(bytes, "application/pdf", "CAUB2");
+
+        }
 
     }
 }
